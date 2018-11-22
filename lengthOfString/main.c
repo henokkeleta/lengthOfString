92ef62575718b63b62e9bb0b5e3b7083e911845d
Henok Berhe <henokberhe@henoks-macbook-pro.local>
2018-11-22 10:09:33 -0500

    Initial Commit

---
+++ lengthOfString/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  lengthOfString
+//
+//  Created by Henok Berhe on 11/22/18.
+//  Copyright © 2018 Henok Berhe. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
